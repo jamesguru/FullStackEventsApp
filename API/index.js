bf5cf303562b53dc0a790e3803d4b56d7ae4b2c8
unknown <jameskagunga2022@gmail.com>
2023-08-02 02:41:38 -0700

    changes added

--- API/index.js
+++ API/index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-const PORT= process.env.PORT;
+const PORT = process.env.PORT;
+const DB = process.env.DB;
 
-app.listen(PORT,()=>{
+mongoose.connect(DB)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
---
+++ API/models/Events.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const EventSchema = mongoose.Schema({
+  img: { type: String },
+  date: { type: String },
+  desc: { type: String },
+  price: { type: Number },
+  location: { type: String },
+  eventType: { type: String },
+  timetable: { type: Array },
+  others: { type: Array },
+  ticket: { type: String },
+  video: { type: String },
+  overview: { type: String },
+});
+
+module.exports=mongoose.model("Event",EventSchema)
